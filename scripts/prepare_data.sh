d7264fbde24df942fd85ae848f1fc07486bba7d1
mkshing <mktshing4@gmail.com>
2023-04-28 23:20:37 +0900

    fix prepare_data.sh

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
-#SBATCH --account="stability"
-#SBATCH --job-name="jp-stable-lm"
-#SBATCH --partition=g40
+#SBATCH --job-name="jp-mc4"
+#SBATCH --partition=cpu128
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=12
-#SBATCH --output=/fsx/home-mkshing/slurm_outs/neox_%j.out
-#SBATCH --error=/fsx/home-mkshing/slurm_outs/neox_%j.err
+#SBATCH --ntasks-per-node 1
+#SBATCH --cpus-per-task=64
+#SBATCH --exclusive
+#SBATCH --mem=0 #0 means use all available memory (in MB)
+#SBATCH --output=/fsx/home-mkshing/slurm_outs/%x_%j.out
+#SBATCH --error=/fsx/home-mkshing/slurm_outs/%x_%j.err
+#SBATCH --comment stability
 
 module load openmpi cuda/11.7
-micromamba activate stable-neox-env
+micromamba activate stable-neox-env-cpu
 
 python /fsx/home-mkshing/code/jp-StableLM/gpt-neox/tools/preprocess_data.py \
-    --input /fsx/jp-llm/polyglot-ja/2_quality_filter/v2/cc-100 \
-    --output-prefix /fsx/jp-llm/merged/cc-100/cc-100 \
+    --input /fsx/jp-llm/polyglot-ja/2_quality_filter/v2/mc4 \
+    --output-prefix /fsx/jp-llm/merged/mc4/mc4 \
     --vocab-file /fsx/jp-llm/novelai.model \
     --dataset-impl mmap \
     --tokenizer-type SPMTokenizer \
+    --workers 128 \
     --append-eod
