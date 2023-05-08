923b4f28f52640ac3e6ec613b0672e3d96e34f0c
mkshing <mktshing4@gmail.com>
2023-05-08 15:49:13 +0900

    fix evalute.sh

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -1,21 +1,24 @@
-# python ./deepy.py evaluate.py -d configs your_configs.yml --eval_tasks jcommonsenseqa jsquadqa jaquadqa
+#!/bin/bash
+#SBATCH --account="stablegpt"
+#SBATCH --job-name="jp-eval"
+#SBATCH --partition=g40
+#SBATCH --cpus-per-task=12
+#SBATCH --gpus=1
+#SBATCH --output=/fsx/home-mkshing/slurm_outs/%x_%j.out
+#SBATCH --error=/fsx/home-mkshing/slurm_outs/%x_%j.err
 
-# What's changed
-# - Apply JGLUE (with japanese prompt like "タイトル: {title} 背景: {context} 質問: {question} 回答: {answer}"). For more details, please see `lm_eval/tasks/jglue.py`. 
-# - Enable tokenizer to set use_fast in gpt.py
-# - remove invalid data inside JSQuAD (see `validation_docs` func in jglue.py)
+
 MODEL_ARGS="pretrained=abeja/gpt-neox-japanese-2.7b,low_cpu_mem_usage=True"
 # MODEL_ARGS="pretrained=rinna/japanese-gpt-1b,use_fast=False"
 # MODEL_ARGS="pretrained=naclbit/gpt-j-japanese-6.8b,low_cpu_mem_usage=True"
-TASK="jsquad" # jsquad, jaquad, jcommonsenseqa
-# source /fsx/home-mkshing/venv/nlp/bin/activate
+TASK="jsquad,jaquad" # jsquad, jaquad, jcommonsenseqa
+source /fsx/home-mkshing/venv/nlp/bin/activate
 python scripts/lm_harness_main.py \
     --model hf-causal \
     --model_args $MODEL_ARGS \
     --tasks $TASK \
     --num_fewshot 2 \
     --device "cuda" \
-    # --no_cache
 
 # [Example] From gpt-neox's checkpoints
 # python ./deepy.py evaluate.py \
